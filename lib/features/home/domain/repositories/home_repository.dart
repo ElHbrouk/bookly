c35a1634f8f3af7490fb3f656a0b76d429417e34
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-04-07 15:05:40 +0200

    add Either class

--- lib/features/home/domain/repositories/home_repository.dart
+++ lib/features/home/domain/repositories/home_repository.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepository {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either< Failure,List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either< Failure,List<BookEntity>>> fetchNewestBooks();
 }
