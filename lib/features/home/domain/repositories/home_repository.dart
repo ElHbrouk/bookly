930afac0fc7a3205cb712fdd52be45f377a41632
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-04-07 13:48:42 +0200

    adding fetching methods

--- lib/features/home/domain/repositories/home_repository.dart
+++ lib/features/home/domain/repositories/home_repository.dart
@@ -1,4 +1,6 @@
-abstract class HomeRepository
-{
-  
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRepository {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
 }
