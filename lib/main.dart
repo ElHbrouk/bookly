483ea8d34e9c137136d17246971799e5693ab3c0
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-05-26 14:24:17 +0300

    Initialize Hive database and register adapters for BookEntity in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox<BookEntity>(kFeaturedBox);
+  await Hive.openBox<BookEntity>(kNewestBox);
   runApp(const BooklyApp());
 }
 
@@ -19,7 +26,8 @@ class BooklyApp extends StatelessWidget {
         useMaterial3: true,
       ).copyWith(
         textTheme: GoogleFonts.montserratAlternatesTextTheme(
-            ThemeData.dark().textTheme),
+          ThemeData.dark().textTheme,
+        ),
         appBarTheme: const AppBarTheme(
           color: kPrimaryColor,
         ),
