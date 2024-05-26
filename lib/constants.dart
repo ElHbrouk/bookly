d8ea03051adca3e34f193f4c3856ba4c76b84351
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-05-26 14:23:48 +0300

    Add constants for featured and newest boxes.

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff100b20);
 const kTransitionDuration = Duration(milliseconds: 300);
 const kFontFamily = 'Lora';
+const kFeaturedBox = 'featuredBox';
+const kNewestBox = 'newestBox';
