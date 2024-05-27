ad2b3aedfb0ca2fc0a4a9d25678195f312ea9d96
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-05-27 06:57:40 +0300

    Add implementation for fetching featured and newest books
    Implemented the fetchFeaturedBooks and fetchNewestBooks methods in HomeRepositoryImp class to fetch books from both local and remote data sources. If books are available in the local data source, they are returned, otherwise books are fetched from the remote data source. In case of any error, a Failure object is returned.

--- lib/features/home/data/repositories/home_repository_imp.dart
+++ lib/features/home/data/repositories/home_repository_imp.dart
@@ -1,19 +1,42 @@
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/home/data/data_sorces/home_local_data_source.dart';
+import 'package:bookly/features/home/data/data_sorces/home_remote_data_source.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/repositories/home_repository.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepositoryImp extends HomeRepository {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepositoryImp(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> booksLocal = homeLocalDataSource.fetchFeaturedBooks();
+      if (booksLocal.isNotEmpty) {
+        return right(booksLocal);
+      }
+      List<BookEntity> books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      List<BookEntity> booksLocal = homeLocalDataSource.fetchNewestBooks();
+      if (booksLocal.isNotEmpty) {
+        return right(booksLocal);
+      }
+      List<BookEntity> books = await homeRemoteDataSource.fetchNewestBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure());
+    }
   }
-
 }
