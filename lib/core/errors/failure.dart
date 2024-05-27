61ee3c72ce87f3242b8644e28296ebed7ea353d3
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-05-27 09:52:30 +0300

    Add error handling for DioExceptions in Failure class
    Added import statement for Dio package and implemented two factory methods in the Failure class to handle DioExceptions. The first factory method handles different types of DioExceptions and returns a ServerFailure object with an appropriate error message. The second factory method handles different HTTP status codes and returns a ServerFailure object with an appropriate error message. This commit improves error handling in the Failure class.

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+
 abstract class Failure {
   final String message;
 
@@ -6,4 +8,36 @@ abstract class Failure {
 
 class ServerFailure extends Failure {
   ServerFailure({required super.message});
+  factory ServerFailure.fromDioError(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(message: "Connection Timeout");
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(message: "Send Timeout");
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(message: "Receive Timeout");
+      case DioExceptionType.badCertificate:
+        return ServerFailure(message: "Bad Certificate");
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioException.response!.statusCode, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure(message: "Cancel");
+      case DioExceptionType.connectionError:
+        return ServerFailure(message: "Connection Error");
+      case DioExceptionType.unknown:
+        return ServerFailure(message: " Oops, Unknown Error");
+    }
+  }
+  factory ServerFailure.fromResponse(int? statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(message: response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure(message: "Not Found , please try later");
+    } else if (statusCode == 500) {
+      return ServerFailure(message: "Internal Server Error");
+    } else {
+      return ServerFailure(message: "Oops, something went wrong");
+    }
+  }
 }
