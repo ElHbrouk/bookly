04fbf9929c8156a7b9ac71a0808712bdc2fe957e
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-05-28 08:42:26 +0300

    Refactor main.dart to use BlocProvider and FetchUseCase
    The main.dart file has been refactored to use BlocProvider and FetchUseCase for both FeaturedBooks and NewestBooks. This will improve the code structure and make it easier to maintain in the future. Additionally, a SimpleBlocObserver has been added to observe the state changes in the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/functions/service_locator.dart';
+import 'package:bookly/core/utils/simple_bloc_observer.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/repositories/home_repository.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 
@@ -11,6 +19,7 @@ void main() async {
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox<BookEntity>(kFeaturedBox);
   await Hive.openBox<BookEntity>(kNewestBox);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const BooklyApp());
 }
 
@@ -19,22 +28,40 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Bookly App',
-      darkTheme: ThemeData.dark(
-        useMaterial3: true,
-      ).copyWith(
-        textTheme: GoogleFonts.montserratAlternatesTextTheme(
-          ThemeData.dark().textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            FetchFeaturedBooksUseCase(
+              homeRepository: getIt.get<HomeRepository>(),
+            ),
+          ),
         ),
-        appBarTheme: const AppBarTheme(
-          color: kPrimaryColor,
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            FetchNewestBooksUseCase(
+              homeRepository: getIt.get<HomeRepository>(),
+            ),
+          ),
         ),
-        scaffoldBackgroundColor: kPrimaryColor,
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'Bookly App',
+        darkTheme: ThemeData.dark(
+          useMaterial3: true,
+        ).copyWith(
+          textTheme: GoogleFonts.montserratAlternatesTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+          appBarTheme: const AppBarTheme(
+            color: kPrimaryColor,
+          ),
+          scaffoldBackgroundColor: kPrimaryColor,
+        ),
+        themeMode: ThemeMode.dark,
+        routerConfig: AppRouter.router,
       ),
-      themeMode: ThemeMode.dark,
-      routerConfig: AppRouter.router,
     );
   }
 }
