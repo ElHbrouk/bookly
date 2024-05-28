70f973c6d7527e6b35119849e6bd06b8231ee80e
ElHbrouk <omar.elhbrouk2@gmail.com>
2024-05-28 08:42:10 +0300

    Refactor HomeLocalDataSourceImpl class
    Removed unused import and constructor parameter in HomeLocalDataSourceImpl class.

--- lib/features/home/data/data_sorces/home_local_data_source.dart
+++ lib/features/home/data/data_sorces/home_local_data_source.dart
@@ -1,6 +1,5 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
-import 'package:bookly/features/home/domain/repositories/home_repository.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 abstract class HomeLocalDataSource {
@@ -9,9 +8,7 @@ abstract class HomeLocalDataSource {
 }
 
 class HomeLocalDataSourceImpl extends HomeLocalDataSource {
-  final HomeRepository homeRepository;
 
-  HomeLocalDataSourceImpl({required this.homeRepository});
   var box;
   @override
   List<BookEntity> fetchFeaturedBooks() {
